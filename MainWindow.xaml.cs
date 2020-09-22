2469b7d92a3fdeeaef6d3a74b843f59a11e2e44a
Mikhail <wowivan2011@yandex.ru>
2020-09-22 14:04:03 +0300

    Resize project to 1280x720

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // <copyright file="MainWindow.xaml.cs" company="Microsoft">
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
